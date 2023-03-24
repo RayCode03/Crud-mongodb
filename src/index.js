89398b5b1e8100d3e5e517af6c75b24320a34678
RayCode03 <raymonddrg03@gmail.com>
2023-03-24 16:51:23 -0400

    update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
-const userRoutes = require("./routes/user")
-
+const userRoutes = require("./routes/user");
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -11,21 +10,19 @@ const port = process.env.PORT || 4000;
 app.use(express.json());
 app.use("/api", userRoutes);
 
-
 // router
 
-app.get('/', (req, res) => {
-
-    res.send("Welcome to my API REST");
-
+app.get("/", (req, res) => {
+  res.send("Welcome to my API REST");
 });
 
-// mongoosedb connected 
+// mongoosedb connected
 
-mongoose.connect(process.env.MONGOOSE_DBA).then(() => console.log("Connected to MongooseDBA Atlas")).catch((error) => console.error(error));
+mongoose
+  .connect(process.env.MONGOOSE_DBA)
+  .then(() => console.log("Connected to MongooseDBA Atlas"))
+  .catch((error) => console.error(error));
 
-
-
-app.listen(port, ()=>{
-    console.log("Server listening on port", port);
+app.listen(port, () => {
+  console.log("Server listening on port", port);
 });
--- src/routes/user.js
+++ src/routes/user.js
@@ -7,40 +7,53 @@ const router = express.Router();
 
 // create user
 
-router.post("/users", (req, res)=>{
-    const user = userSchema(req.body);
-    user.save().then((dato) => res.json(dato)).catch((error) => res.json({message: error}))
+router.post("/users", (req, res) => {
+  const user = userSchema(req.body);
+  user
+    .save()
+    .then((dato) => res.json(dato))
+    .catch((error) => res.json({ message: error }));
 });
 
 // get all users
 
-router.get("/users", (req, res)=>{
-    userSchema.find().then((dato) => res.json(dato)).catch((error) => res.json({message: error}))
+router.get("/users", (req, res) => {
+  userSchema
+    .find()
+    .then((dato) => res.json(dato))
+    .catch((error) => res.json({ message: error }));
 });
-
 
 // get a user
 
-router.get("/users/:id", (req, res)=>{
-    const { id } = req.params;
-    userSchema.findById(id).then((dato) => res.json(dato)).catch((error) => res.json({message: error}))
+router.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+  userSchema
+    .findById(id)
+    .then((dato) => res.json(dato))
+    .catch((error) => res.json({ message: error }));
 });
-
 
 // get a user
 
-router.put("/users/:id", (req, res)=>{
-    const { id } = req.params;
-    const {name, age, email} = req.body;
-    userSchema.updateOne({_id: id}, {$set: {name, age, email}}).then((dato) => res.json(dato)).catch((error) => res.json({message: error}))
+router.put("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const { name, age, email } = req.body;
+  userSchema
+    .updateOne({ _id: id }, { $set: { name, age, email } })
+    .then((dato) => res.json(dato))
+    .catch((error) => res.json({ message: error }));
 });
 
 // delete a user
 
-router.delete("/users/:id", (req, res)=>{
-    const { id } = req.params;
-   
-    userSchema.remove({_id: id}).then((dato) => res.json(dato)).catch((error) => res.json({message: error}))
+router.delete("/users/:id", (req, res) => {
+  const { id } = req.params;
+
+  userSchema
+    .remove({ _id: id })
+    .then((dato) => res.json(dato))
+    .catch((error) => res.json({ message: error }));
 });
 
 module.exports = router;
